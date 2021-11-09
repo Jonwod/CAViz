f955e7077142b387805fb765e01474539b4cabe6
Jonathan Wood <jonathan.m.wood95@gmail.com>
2021-11-09 14:28:26 +0000

    Basic 3d vector class (mainly for type annotations)

---
+++ src/vec3.ts
@@ -0,0 +1,11 @@
+export class Vec3 {
+    constructor(x: number, y: number, z: number) {
+        this.x = x; 
+        this.y = y;
+        this.z = z;
+    }
+
+    public x: number;
+    public y: number;
+    public z: number;
+}
