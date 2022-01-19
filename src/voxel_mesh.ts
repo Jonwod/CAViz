3e33447dda220042aa6bbd68ef0d2d2205d87ec0
Jonathan Wood <jonathan.m.wood95@gmail.com>
2022-01-19 10:25:05 +0000

    Added VAO to voxel mesh

--- src/voxel_mesh.ts
+++ src/voxel_mesh.ts
@@ -31,6 +31,8 @@ export class VoxelMesh {
         fragmentShaderSource: String;
     };
 
+    private vao: WebGLVertexArrayObject;
+
     private gl: WebGL2RenderingContext;
     private gridSize: number;
     private voxelSize: number;
@@ -156,6 +158,10 @@ export class VoxelMesh {
             readBuffer: gl.getUniformLocation(this.programInfo.program, 'uReadBuffer')
         };
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
+
+        this.vao = gl.createVertexArray();
+        gl.bindVertexArray(this.vao);
 
         const positionBuffer = this.gl.createBuffer();
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
@@ -184,14 +190,6 @@ export class VoxelMesh {
                 this.programInfo.attribLocations.vertexPosition);
         }
 
-        const indexBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-        this.gl.bufferData(
-            this.gl.ELEMENT_ARRAY_BUFFER,
-            new Uint16Array(indices),
-            this.gl.STATIC_DRAW
-        );
-
         const normalBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer); 
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
@@ -216,7 +214,16 @@ export class VoxelMesh {
                 this.programInfo.attribLocations.vertexNormal);
         }
 
-        // Unbind buffer
+        const indexBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        this.gl.bufferData(
+            this.gl.ELEMENT_ARRAY_BUFFER,
+            new Uint16Array(indices),
+            this.gl.STATIC_DRAW
+        );
+
+        // Unbind
+        gl.bindVertexArray(null);
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
         this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, null);
 
@@ -236,7 +243,6 @@ export class VoxelMesh {
         const gl = this.gl;
 
         gl.useProgram(programInfo.program);
-        
 
         const normalMatrix = mat4.create();
         mat4.invert(normalMatrix, modelViewMatrix);
@@ -270,6 +276,7 @@ export class VoxelMesh {
         gl.bindTexture(gl.TEXTURE_2D, cellDataTexture);
         gl.uniform1i(this.programInfo.uniformLocations.readBuffer, 0);
 
+        gl.bindVertexArray(this.vao);
         {
             const offset = 0;
             // gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
@@ -277,6 +284,7 @@ export class VoxelMesh {
             const instanceCount = this.gridSize ** 3;
             gl.drawElementsInstanced(gl.TRIANGLES, buffers.indexCount, gl.UNSIGNED_SHORT, offset, instanceCount);
         }
+        gl.bindVertexArray(null);
     }
 
     private initBuffers(): {positions: number[], indices: number[], normals: number[]} {
