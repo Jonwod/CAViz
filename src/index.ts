596c7e47f1709b281a021fd51f35ae93298d711e
Jonathan Wood <jonathan.m.wood95@gmail.com>
2022-02-02 20:08:57 +0000

    part way through adding UI for CA spec. Increadibly tedious....

--- src/index.ts
+++ src/index.ts
@@ -16,51 +16,194 @@ let appStateMachine: StateMachine;
 class ConstructionState extends State {
     onEnter(): void {
         let div = document.createElement("div");
-        let button = document.createElement("button");
-        button.innerHTML = "2D Cellular Automaton";
-        button.addEventListener("click", () => {
-            appStateMachine.setState(new SimState(false));
+
+        // let button = document.createElement("button");
+        // button.innerHTML = "2D Cellular Automaton";
+        // button.addEventListener("click", () => {
+        //     appStateMachine.setState(new SimState(false));
+        // });
+        // div.appendChild(button);
+
+        // let button2 = document.createElement("button");
+        // button2.addEventListener("click", () => {
+        //     appStateMachine.setState(new SimState(true));
+        // });
+        // button2.innerHTML = "3D Cellular Automaton";
+        // div.appendChild(button2);
+
+        let that = this;
+
+        // let form = document.createElement('form');
+        {
+            let dimensionalityDiv = document.createElement("div");
+
+            let button2d = document.createElement("input");
+            button2d.setAttribute("type", "radio");
+            button2d.setAttribute("name", "dim");
+            button2d.value = "2d";
+            button2d.id = "radio2d";
+            dimensionalityDiv.appendChild(button2d);
+
+            let label2d = document.createElement("label");
+            label2d.innerHTML = "2D";
+            label2d.setAttribute("for", button2d.id);
+            dimensionalityDiv.appendChild(label2d);
+
+            let button3d = document.createElement("input");
+            button3d.setAttribute("type", "radio");
+            button3d.setAttribute("name", "dim");
+            button3d.value = "3d";
+            dimensionalityDiv.appendChild(button3d);
+
+            let label3d = document.createElement("label");
+            label3d.innerHTML = "3D";
+            label3d.setAttribute("for", button3d.id);
+            dimensionalityDiv.appendChild(label3d);
+
+            button2d.addEventListener("change", (event) => {
+                that.dimensions = 2;
+                console.log('2');
+            });
+
+            button3d.addEventListener("change", (event) => {
+                that.dimensions = 3;
+                console.log('3');
+            });
+
+            button3d.checked = true;
+            this.dimensions = 3;
+
+            div.appendChild(dimensionalityDiv);
+        }
+
+        {
+            let totalisticParamsDiv = document.createElement("div");
+
+            let stayAliveInputLow = document.createElement("input");
+            stayAliveInputLow.addEventListener("change", (e) => {
+                // TODO: validate
+                let input = e.target as HTMLInputElement;
+                that.keepAliveLower = parseInt(input.value);
+            });
+
+            let stayAliveInputHigh = document.createElement("input");
+            stayAliveInputHigh.addEventListener("change", (e) => {
+                // TODO: validate
+                let input = e.target as HTMLInputElement;
+                that.keepAliveUpper = parseInt(input.value);
+            });
+
+            let reproduceInputLow = document.createElement("input");
+            reproduceInputLow.addEventListener("change", (e) => {
+                // TODO: validate
+                let input = e.target as HTMLInputElement;
+                that.reproduceLower = parseInt(input.value);
+            });
+
+            let reproduceInputHigh = document.createElement("input");
+            reproduceInputHigh.addEventListener("change", (e) => {
+                // TODO: validate
+                let input = e.target as HTMLInputElement;
+                that.reproduceUpper = parseInt(input.value);
+            });
+
+            [   stayAliveInputLow, 
+                stayAliveInputHigh, 
+                reproduceInputLow, 
+                reproduceInputHigh].forEach((inputField) => {
+                    inputField.setAttribute("type", "number");
+                    inputField.setAttribute("min", "0");
+            });
+
+            let aliveRangeLable = document.createElement("p");
+            aliveRangeLable.innerHTML = "<b>Stay-alive range</b>";
+            totalisticParamsDiv.appendChild(aliveRangeLable);
+            // totalisticParamsDiv.appendChild(document.createElement("br"));  
+            let aliveRangeExpl = document.createElement("p");
+            aliveRangeExpl.innerHTML = "(Live cell will remain alive when the number of live neighbours falls in this range)";
+            totalisticParamsDiv.appendChild(aliveRangeExpl);
+        
+            totalisticParamsDiv.appendChild(stayAliveInputLow);
+            totalisticParamsDiv.appendChild(stayAliveInputHigh);
+
+            let reproduceRangeLabel = document.createElement("p");
+            reproduceRangeLabel.innerHTML = "<b>Reproduce range</b>";
+            totalisticParamsDiv.appendChild(reproduceRangeLabel);
+            // totalisticParamsDiv.appendChild(document.createElement("br"));  
+            let reproduceRangeExpl = document.createElement("p");
+            reproduceRangeExpl.innerHTML = "(Dead cell becomes alive when the number of live neighbours falls in this range)";
+            totalisticParamsDiv.appendChild(reproduceRangeExpl);
+
+            totalisticParamsDiv.appendChild(reproduceInputLow);
+            totalisticParamsDiv.appendChild(reproduceInputHigh);
+
+            div.appendChild(totalisticParamsDiv);
+        }
+        // TODO: World size
+        // TODO: Pop density
+
+        let confirmButton = document.createElement("button");
+        confirmButton.innerText = "Simulate";
+        confirmButton.addEventListener("click", () => {
+            // TODO: Parameterize no. states
+            const numStates = 2;
+
+            let transitionRule = transitionRuleFromBaysCoding(that.dimensions, 
+                new Range(that.keepAliveLower, that.keepAliveUpper), 
+                new Range(that.reproduceLower, that.reproduceUpper)
+            );
+            let ca = new CellularAutomaton(numStates, that.dimensions, transitionRule);
+            let conf = Configuration.makeRandom(that.dimensions, that.size, numStates, that.popDensity);
+            appStateMachine.setState(new SimState(ca, conf));
         });
-        div.appendChild(button);
-        let button2 = document.createElement("button");
-        button2.addEventListener("click", () => {
-            appStateMachine.setState(new SimState(true));
-        });
-        button2.innerHTML = "3D Cellular Automaton";
-        div.appendChild(button2);
+        div.appendChild(confirmButton);
+
         this.myHTML = div;
         document.getElementsByTagName("body")[0].appendChild(this.myHTML);
     }
+
+    // if(this.threeD) {
+    //     // Bays' (5766) rule:
+    //     const nStates = 2;
+    //     const nDimensions = 3;
+
+    //     const transitionRule = transitionRuleFromBaysCoding( nDimensions, new Range(4,5), new Range(2,6));
+
+    //     let testCA = new CellularAutomaton(nStates, nDimensions, transitionRule);
+    //     let config = Configuration.makeRandom(3, 130, 2, 0.34);
+    //     sim = createSimulation(testCA, config);
+    // } else {
+    //     const transitionRule = transitionRuleFromBaysCoding(2, new Range(2,3), new Range(3,3));
+    //     let life2d = new CellularAutomaton(2, 2, transitionRule);
+    //     let initConfig = Configuration.makeRandom(2, 512, 2, 0.4);
+    //     sim = createSimulation(life2d, initConfig);
+    // }
+
+
     onExit(): void {
         this.myHTML.remove();
     }
 
     private myHTML: HTMLElement;
+
+    private dimensions: number;
+    private size: number = 100;
+    private popDensity: number = 0.34;
+    private keepAliveLower: number;
+    private keepAliveUpper: number;
+    private reproduceLower: number;
+    private reproduceUpper: number;
 }
 
 class SimState extends State {
-    constructor(threeD: boolean) {
+    constructor(ca: CellularAutomaton, initConfig: Configuration) {
         super();
-        this.threeD = threeD;
+        this.ca = ca;
+        this.initConfig = initConfig;
     }
+
     onEnter(): void {
-        let sim: CASimulation;
-        if(this.threeD) {
-            // Bays' (5766) rule:
-            const nStates = 2;
-            const nDimensions = 3;
-
-            const transitionRule = transitionRuleFromBaysCoding( nDimensions, new Range(4,5), new Range(2,6));
-
-            let testCA = new CellularAutomaton(nStates, nDimensions, transitionRule);
-            let config = Configuration.makeRandom(3, 100, 2, 0.34);
-            sim = createSimulation(testCA, config);
-        } else {
-            const transitionRule = transitionRuleFromBaysCoding(2, new Range(2,3), new Range(3,3));
-            let life2d = new CellularAutomaton(2, 2, transitionRule);
-            let initConfig = Configuration.makeRandom(2, 512, 2, 0.4);
-            sim = createSimulation(life2d, initConfig);
-        }
+        let sim: CASimulation = createSimulation(this.ca, this.initConfig);
 
         let div = document.createElement("div");
         this.myHTML = div; 
@@ -80,7 +223,8 @@ class SimState extends State {
     }
 
     private myHTML: HTMLElement;
-    private threeD: boolean;
+    private ca: CellularAutomaton;
+    private initConfig: Configuration;
 }
 
 class TestInstancedRender extends State {
@@ -102,4 +246,4 @@ class TestInstancedRender extends State {
 }
 
 
-appStateMachine = new StateMachine(new SimState(true));
+appStateMachine = new StateMachine(new ConstructionState());
