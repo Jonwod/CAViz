8d43e3efd9bb0e8e0bc134f94d2415726de3d8fc
Jonathan Wood <jonathan.m.wood95@gmail.com>
2022-03-19 16:40:42 +0000

    docs

--- src/assert.ts
+++ src/assert.ts
@@ -1,3 +1,8 @@
+/**
+ * If the condition is false, this will throw an error and produce an alert window.
+ * @param condition If this evaluates to false, assert fails
+ * @param errorMessage Custom message to be presented on assert failure
+ */
 export function assert(condition: boolean, errorMessage?: string) {
     if(!condition) {
         alert("Assertation failed: " + errorMessage);
