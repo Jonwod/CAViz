85df8753641514aee0a49340322e9adafca4ae13
Jonathan Wood <jonathan.m.wood95@gmail.com>
2022-03-20 10:46:46 +0000

    More informative test failure messages

--- src/test.ts
+++ src/test.ts
@@ -16,14 +16,14 @@ function assert(condition: boolean, errorMessage?: string) {
 
 
 export function runAllTests() {
-    assert(factorial(1) === 1);
+    assert(factorial(1) === 1, `factorial(1) = ${factorial(1)}`);
     assert(factorial(2) === 2, `factorial(2) = ${factorial(2)}`);
-    assert(factorial(6) === 720);
+    assert(factorial(6) === 720,`factorial(6) = ${factorial(6)}`);
 
-    assert(nChooseK(1, 1) === 1);
-    assert(nChooseK(10, 4) === 210);
+    assert(nChooseK(1, 1) === 1, `nChooseK(1, 1)  = ${nChooseK(1, 1) }`);
+    assert(nChooseK(10, 4) === 210, `nChooseK(1, 1)  = ${nChooseK(10, 4)}`);
 
-    assert(trueMod(7, 5) === 2);
-    assert(trueMod(-7, 5) === 3);
+    assert(trueMod(7, 5) === 2, `trueMod(7, 5) = ${trueMod(7, 5)}`);
+    assert(trueMod(-7, 5) === 3, `trueMod(-7, 5) = ${trueMod(-7, 5)}`);
 }
 
